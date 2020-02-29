3f82b5f53b327321f936b57e60a1a6706d9e9db8
Kathy Tran <44452243+katran009@users.noreply.github.com>
2020-02-28 19:25:53 -0600

    Add files via upload

--- nested.py
+++ nested.py
@@ -3,20 +3,64 @@
 """
 Module docstring: One line description of what your program does.
 """
-__author__ = "???"
+__author__ = "katran009, Demo by Piero"
 
 import sys
 
+# openers = {"(", "[", "{", "<", "*"}
+# closers = {")", "]", "}", ">", "*"}
+
+bracket_dict = {
+    ")" : "(",
+    "]" : "[",
+    "}" : "{",
+    ">" : "<",
+    "*)" : "(*",
+}
 
 def is_nested(line):
     """Validate a single input line for correct nesting"""
-    pass
+    stack = []
+    counter = 0
+    while line:
+        # figure out what my token is this time thru the loop
+        token = line[0]
+        if line.startswith("(*"):
+            token = "(*"
+        elif line.startswith("*)"):
+            token = "*)"
 
+        token_counter += 1
+        line - line[len(token):]
+
+        if token in tokens_dict.values():
+            stack.append(token)
+        elif token in tokens_dict.keys():
+            # closer_index = closrs.index(token)
+            expected_opener = tokens_dict(token)
+            if stack.pop() != expected_opener:
+                return "NO " + str(token_counter)
+
+# while loop has new exited
+# check if there is anything left on the stack
+if stack:
+    # we haev junk left over, this is bad
+    return "NO " + str(token_counter)
+
+# If we make it here, all is GOOD
+return "YES "
 
 def main(args):
     """Open the input file and call `is_nested()` for each line"""
     # Results: print to console and also write to output file
-    pass
+    print("Testing for Nesting: {}".format(args[0]))
+    with open(args[0]) as ifile:
+        with open('output.txt', 'w') as ofile:
+            for line in ifile:
+                result_str = is_nested(line)
+                # result_str = "NO {}".format(index) if index else "YES"
+                print(result_str)
+                ofile.write(result_str + '\n')
 
 
 if __name__ == '__main__':
